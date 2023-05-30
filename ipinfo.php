30b0cde77b1141c1318b9d141acb5cc3df32fa33
al0x99 <a.sfirschi@gmail.com>
2023-05-30 20:08:55 +0200

    esternalizza il carico

--- ipinfo.php
+++ ipinfo.php
@@ -2,62 +2,34 @@
 /*
 Plugin Name: Salva indirizzo IP e informazioni
 Plugin URI: https://wpaper.it
-Description: Salva l'indirizzo IP e le informazioni dell'utente che visita il sito
-Version: 0.2   
+Description: Salva l'indirizzo IP e le informazioni dell'utente e le invia ad un server .net
+Version: 0.3
 */
 
-// Attivazione plugin
-register_activation_hook( __FILE__, 'create_ip_info_table' );
-function create_ip_info_table() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'ip_info';
+add_action( 'init', 'send_ip_and_url' );
 
-    $charset_collate = $wpdb->get_charset_collate();
-
-    $sql = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        ip varchar(100) NOT NULL,
-        country varchar(100) NOT NULL,
-        asn_name varchar(100) NOT NULL,
-        company_name varchar(100) NOT NULL,
-        company_domain varchar(100) NOT NULL,
-        PRIMARY KEY  (id)
-    ) $charset_collate;";
-
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
-}
-
-// Salva le informazioni dell'IP ad ogni richiesta
-add_action( 'init', 'save_ip_info' );
-function save_ip_info() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'ip_info';
-    $ip = $_SERVER['REMOTE_ADDR'];
-
-    $existing = $wpdb->get_var( $wpdb->prepare( "SELECT ip FROM $table_name WHERE ip = %s LIMIT 1", $ip ) );
-    if ( $existing !== null ) {
-        return;
+function send_ip_and_url() {
+    if (!session_id()) {
+        session_start();
     }
 
-    $request = new WP_Http;
-    $response = $request->request( "https://ipinfo.io/$ip?token=eeb8e42eb47124" );
-    if ( is_wp_error( $response ) ) {
-        // gestisci l'errore qui
-        return;
+    if (!isset($_SESSION['ip_and_url_sent'])) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $url = home_url( add_query_arg( null, null ) );
+
+        $response = wp_remote_post( 'http://195.32.71.172:3000/ipinfo', array(
+            'method' => 'POST',
+            'headers' => array('Content-Type' => 'application/json; charset=utf-8'),
+            'body' => json_encode(array('ip' => $ip, 'url' => $url)),
+            'sslverify' => false,
+            'data_format' => 'body'
+        ));
+
+        if ( is_wp_error( $response ) ) {
+            error_log( $response->get_error_message() );
+        }
+
+        $_SESSION['ip_and_url_sent'] = true;
     }
-
-    $data = json_decode( wp_remote_retrieve_body( $response ), true );
-
-    $wpdb->insert(
-        $table_name,
-        array(
-            'ip' => $ip,
-            'country' => $data['country'],
-            'asn_name' => $data['asn']['name'],
-            'company_name' => $data['company']['name'],
-            'company_domain' => $data['company']['domain']
-        )
-    );
 }
 ?>
